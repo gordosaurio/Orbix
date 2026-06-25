3e4273ff10032d786edcdd9b5330cc814e45dd5c
GordoSaurio​ <andres.santa-fe@hotmail.com>
2026-06-25 08:34:31 -0500

    fix camera controls

--- OrbixFronted/orbix-frontend/src/scene/controls/CameraControls.tsx
+++ OrbixFronted/orbix-frontend/src/scene/controls/CameraControls.tsx
@@ -1,23 +1,23 @@
 import { OrbitControls } from '@react-three/drei'
 
 function CameraControls() {
-  return (
-    <OrbitControls
-      enablePan
-      enableZoom
-      enableRotate
-      screenSpacePanning
-      zoomSpeed={0.9}
-      panSpeed={0.9}
-      rotateSpeed={0.55}
-      minDistance={8}
-      maxDistance={120}
-      minPolarAngle={0.15}
-      maxPolarAngle={Math.PI - 0.2}
-      dampingFactor={0.09}
-      enableDamping
-    />
-  )
+    return (
+        <OrbitControls
+            enablePan
+            enableZoom
+            enableRotate
+            screenSpacePanning
+            zoomSpeed={0.9}
+            panSpeed={0.9}
+            rotateSpeed={0.55}
+            minDistance={8}
+            maxDistance={120}
+            minPolarAngle={0.15}
+            maxPolarAngle={Math.PI - 0.2}
+            dampingFactor={0.09}
+            enableDamping
+        />
+    )
 }
 
 export default CameraControls
